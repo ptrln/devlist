fc40ccecb2c656a5a6f443018d765b6d2c6a3aab
Peter Lin <ptrln@me.com>
2013-05-03 15:22:38 -0700

    fixed notification display

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,9 @@
 module DashboardHelper
+  def notification_user_icon(update)  # of notifiable type
+    if update.notifiable_type == "User"
+      update.notifiable.photo ? (update.notifiable.photo.url + filepicker_crop_view(30, 30)) : "default_user_thumb.png"
+    elsif update.notifiable_type == "UserProject"
+      update.notifiable.user.photo ? (update.notifiable.user.photo.url + filepicker_crop_view(30, 30)) : "default_user_thumb.png"
+    end
+  end
 end
