efeed5cc4d518827bf168a521078acb2f0937ec6
Peter Lin <ptrln@me.com>
2013-05-02 22:55:54 -0700

    order skill desc

--- app/models/user_skill.rb
+++ app/models/user_skill.rb
@@ -3,6 +3,8 @@ class UserSkill < ActiveRecord::Base
 
   belongs_to :user
 
+  default_scope order('proficiency DESC')
+
   validates :name, presence: true
   validates :proficiency, presence: true
   validates :user, presence: true
